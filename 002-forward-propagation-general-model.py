88aa93ece1df6b94743a57b41ec9e47cc731e6a5
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2021-11-19 15:55:28 +0100

    Update 002-forward-propagation-general-model.py

--- 002-forward-propagation-general-model.py
+++ 002-forward-propagation-general-model.py
@@ -72,13 +72,13 @@ def initialize_network(num_inputs, num_hidden_layers, num_nodes_hidden, num_node
 #The network initialization function has been defined, let us run this baby and create an aptly named small baby network!
 small_baby_network = initialize_network(5, 3, [3, 2, 3], 1)
 
+##### CREATING A FORWARD PROPAGATION ######
 #we have initialized this network. 
 #Put we do not yet know the weighted sum at each node. 
 #For that we need to compute the dot product of inputs and weights, and then add the bias
 
 def compute_weighted_sum(inputs, weights, bias):
     return np.sum(inputs * weights) + bias
-
 
 #let us seed a random input layer
 from random import seed #yeah, yeah, i know, we already have it in the packages at the top, i just want to see it again
@@ -89,6 +89,49 @@ print('The inputs to the network are {}'.format(inputs))
 #just to see everything works, we will do a check run for the weighted sum computation of the layer 1 node 1
 node_weights = small_baby_network['layer_1']['node_1']['weights']
 node_bias = small_baby_network['layer_1']['node_1']['bias']
-
 weighted_sum = compute_weighted_sum(inputs, node_weights, node_bias)
 print('The weighted sum at the first node in the hidden layer is {}'.format(np.around(weighted_sum[0], decimals=4)))
+
+#everything works. okay. next we need to define a function that does the activation using sigmoid
+def node_activation(weighted_sum):
+    return 1.0 / (1.0 + np.exp(-1 * weighted_sum))
+
+#just to check everything works, check run for the activation function of the layer 1 node 1
+node_activation(weighted_sum)
+
+
+####### PUTTING IT ALL TOGETHER FOR THE FORWARD PROPAGATION #######
+#The final piece of building the neural network that can perform predictions is to put everything together 
+#Create a function that applies the compute_weighted_sum and node_activation functions to each node in the network 
+#and propagates the data all the way to the output layer and outputs a prediction for each node in the output layer
+
+#1. Start with the input layer as the input to the first hidden layer
+#2. Compute the weighted sum at the nodes of the current layer
+#3. Compute the output (after activation function) of the nodes of the current layer
+#4. Set the output of the current layer to be the input to the next layer
+#5. Move to the next layer in the network
+#6. Repeat steps 2 - 4 until we compute the output of the output layer
+
+def forward_propagate(network, inputs):
+    
+    layer_inputs = list(inputs) # start with the input layer as the input to the first hidden layer
+    
+    for layer in network:
+        
+        layer_data = network[layer]
+        layer_outputs = [] 
+        for layer_node in layer_data:
+        
+            node_data = layer_data[layer_node]
+        
+            # compute the weighted sum and the output of each node at the same time 
+            node_output = node_activation(compute_weighted_sum(layer_inputs, node_data['weights'], node_data['bias']))
+            layer_outputs.append(np.around(node_output[0], decimals=4))
+            
+        if layer != 'output':
+            print('The outputs of the nodes in hidden layer number {} is {}'.format(layer.split('_')[1], layer_outputs))
+    
+        layer_inputs = layer_outputs # set the output of this layer to be the input to next layer
+
+    network_predictions = layer_outputs
+    return network_predictions
