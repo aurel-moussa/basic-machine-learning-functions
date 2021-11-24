dda594577eca9d3ad00b23aede8a6375e4c19bfd
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2021-11-24 16:32:45 +0100

    Update 003-regression-neural-network-keras.py

--- 003-regression-neural-network-keras.py
+++ 003-regression-neural-network-keras.py
@@ -2,11 +2,16 @@
 #Simple syntax, high-level API
 #This presupposes you have downloaded + installed Keras framework & underlying framework such as PyTorch
 
-## DATA WRANGLING ##
-#Get datasets
+#Get required libraries
 import pandas as pd
 import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
 
+## DATA WRANGLING ##
+
+#get dataset and...
 #Putting it into a nice Panda dataframe (who doesn't like Pandas?)
 concrete_data = pd.read_csv('https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DL0101EN/labs/data/concrete_data.csv')
 concrete_data.head()
@@ -16,7 +21,7 @@ concrete_data.shape #Note: Only 1000 samples, so overfitting can be an issue her
 concrete_data.describe()
 concrete_data.isnull().sum()
 
-#Splitting data into training and test sets should be done here, but for now....
+#Splitting data into training and test sets should be done here, but for now, I believe Keras actually does that for us....
 #We will just split into X set and Y set
 
 concrete_data_columns = concrete_data.columns
@@ -35,3 +40,26 @@ predictors_norm.head()
 #save number of predictors into a variable, because we will need it later
 n_cols = predictors_norm.shape[1] # number of predictors
 
+### BUILDING THE MODEL ###
+
+# define regression model (sequential (i.e. that is normal), dense (i.e. all nodes connected to all nodes of the next layer))
+# output layer with ONE node, and 50 nodes in the layers before that. Using ReLu, not sigmoid as activation function, because ReLu is better
+def regression_model():
+    # create model
+    model = Sequential()
+    model.add(Dense(50, activation='relu', input_shape=(n_cols,)))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(1))
+    
+    # compile model
+    model.compile(optimizer='adam', loss='mean_squared_error') #adam optimzer instead of typical gradient descent, loss function is the typical mean squared error
+    return model
+  
+### TRAINING & TESTING MODEL ###
+# build the model
+model = regression_model()
+
+# fit the model
+model.fit(predictors_norm, target, validation_split=0.3, epochs=100, verbose=2) #nice one, the validation is being split already!
+
+#FIN#
