c2455c6333f5777128fde2d58b7aa3b3828fb010
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2021-11-24 15:44:40 +0100

    Create 003-regression-neural-network-keras.py

---
+++ 003-regression-neural-network-keras.py
@@ -0,0 +1,29 @@
+#PyTorch and TensorFlow are more powerful, but Keras is easier to use for beginners
+#Simple syntax, high-level API
+#This presupposes you have downloaded + installed Keras framework & underlying framework such as PyTorch
+
+## DATA WRANGLING ##
+#Get datasets
+import pandas as pd
+import numpy as np
+
+#Putting it into a nice Panda dataframe (who doesn't like Pandas?)
+concrete_data = pd.read_csv('https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DL0101EN/labs/data/concrete_data.csv')
+concrete_data.head()
+concrete_data.shape #Note: Only 1000 samples, so overfitting can be an issue here
+
+#Checking for missing values
+concrete_data.describe()
+concrete_data.isnull().sum()
+
+#Splitting data into training and test sets should be done here, but for now....
+#We will just split into X set and Y set
+
+concrete_data_columns = concrete_data.columns
+
+predictors = concrete_data[concrete_data_columns[concrete_data_columns != 'Strength']] # all columns except Strength
+target = concrete_data['Strength'] # Strength column
+
+#checking whether split worked
+predictors.head()
+target.head()
