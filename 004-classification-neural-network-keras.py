eb86df3c4e864e22b5cc106f78352e4f9842daf0
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2021-11-24 17:02:13 +0100

    Create 004-classification-neural-network-keras.py

---
+++ 004-classification-neural-network-keras.py
@@ -0,0 +1,13 @@
+#PyTorch and TensorFlow are more powerful, but Keras is easier to use for beginners
+#Simple syntax, high-level API
+#This presupposes you have downloaded + installed Keras framework & underlying framework such as PyTorch
+
+#Get required libraries
+import pandas as pd
+import numpy as np
+
+### DATA WRANGLING ###
+#We will use MNIST database, Modified National Institute of Standards and Technology database, 
+#large database of handwritten digits that is commonly used for training various image processing systems
+#contains 60,000 training images and 10,000 testing images of digits written by high school students and employees of the United States Census Bureau
+
