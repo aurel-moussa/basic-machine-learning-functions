a3d6ae4aa1d4c9cb9965e534a63aa78623a5fb88
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2021-11-26 11:24:05 +0100

    Create 005-convolutional-neural-network-keras.py

---
+++ 005-convolutional-neural-network-keras.py
@@ -0,0 +1,4 @@
+### CNNs use image inputs, good at detecting objects, classifying images, and other computer vision tasks ###
+# conventional neural networks take (n x 1) vector as input, but...
+# input to a convolutional neural network mostly an (n x m x 1) for grayscale images 
+# or (n x m x 3) for colored images, where the number 3 represents the red, green, and blue components of each pixel in the image
