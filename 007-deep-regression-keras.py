406609b9b38ef74d8af196138c49eea92731a82b
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2022-03-18 15:44:45 +0100

    Update 007-deep-regression-keras.py

--- 007-deep-regression-keras.py
+++ 007-deep-regression-keras.py
@@ -10,7 +10,8 @@ import keras
 import sklearn
 from keras.models import Sequential
 from keras.layers import Dense
-frm sklearn.model_selection import train_test_split 
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import mean_squared_error
 
 ## DATA WRANGLING ##
 
@@ -23,9 +24,6 @@ concrete_data.shape #Note: Only 1000 samples, so overfitting can be an issue her
 #Checking for missing values
 concrete_data.describe()
 concrete_data.isnull().sum()
-
-#Splitting data into training and test sets should be done here, but for now, I believe Keras actually does that for us....
-#We will just split into X set and Y set
 
 concrete_data_columns = concrete_data.columns
 
@@ -40,11 +38,10 @@ target.head()
 predictors_norm = (predictors - predictors.mean()) / predictors.std()
 predictors_norm.head()
 
-#save number of predictors into a variable, because we will need it later
-n_cols = predictors_norm.shape[1] # number of predictors
-
-#
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+#splitting data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(predictors_norm, target, test_size=0.3, random_state=42) #random_state is set for control purposes, in real application it should not be set
+n_cols = X_train.shape[1] # number of predictors
+print(n_cols)
 
 
 ### BUILDING THE MODEL ###
@@ -67,7 +64,45 @@ def regression_model():
 model = regression_model()
 
 # fit the model
-model.fit(predictors_norm, target, validation_split=0.3, epochs=100, verbose=2) #nice one, the validation is being split already!
+#model.fit(X_train, y_train, validation_split=0.3, epochs=100, verbose=2) #here, we can alternatively use Keras train-test split function, but I have split before already
+model.fit(X_train, y_train, epochs=100, verbose=2)
 
+# evaluate the model using the Keras functions can be done like this, will also show you the error:
+#scores = model.evaluate(X_test, y_test, verbose=0)
+#print(scores)
+
+#However, we will do it using scikitlearns's mean_squared_error function, like this:
+
+# create predictions using our model
+y_pred = model.predict(X_test)
+#print(y_pred)
+
+#compare our models predictions with the actual y values
+print ("Mean Squared Error equals")
+mean_squared_error(y_test, y_pred)
+
+#Now we will run this process multiple times, to ensrue that our one-time run is not just a statistical fluke
+
+#we will just store the results in a list
+results_table_mean_squared_error = []
+
+for i in range(3):
+    X_train, X_test, y_train, y_test = train_test_split(predictors_norm, target, test_size=0.3)
+    n_cols = X_train.shape[1] # number of predictors
+    #print(n_cols)
+    model.fit(X_train, y_train, epochs=100, verbose=2)
+    y_pred = model.predict(X_test)
+    error_of_this_run = mean_squared_error(y_test, y_pred)
+    results_table_mean_squared_error.append(error_of_this_run)
+    
+#Evaluation after multiple runs
+#What does our error table look like?
+print(results_table_mean_squared_error)
+
+#Mean error
+print(np.mean(results_table_mean_squared_error))
+
+#SD error
+print(np.std(results_table_mean_squared_error))
+    
 #FIN#
-
